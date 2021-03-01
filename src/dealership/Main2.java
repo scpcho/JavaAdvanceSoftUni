376ed3ee8abe00345cc8c971e4d1d69d27577fcd
scp <scpcho@abv.bg>
2021-03-01 22:14:23 +0200

    update

--- src/dealership/Main2.java
+++
@@ -1,8 +0,0 @@
-package dealership;
-
-public class Main2 {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/javaOOP/WorkingWithAbastractionEx/cardRanks/Main.java
+++ src/javaOOP/WorkingWithAbastractionEx/cardRanks/Main.java
@@ -1,4 +1,16 @@
 package src.javaOOP.WorkingWithAbastractionEx.cardRanks;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(scan.nextLine()+":");
+        for (Ranks value : Ranks.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n",value.ordinal(),value.name());
+        }
+    }
 }
